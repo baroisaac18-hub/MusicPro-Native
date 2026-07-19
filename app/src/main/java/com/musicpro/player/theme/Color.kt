d98d79ccce9e8e974337411a1be3a4e87999f369
baroisaac18-hub <baroisaac18@gmail.com>
2026-07-19 12:38:22 +0100

    Add app/src/main/java/com/musicpro/player/theme/Color.kt

---
+++ app/src/main/java/com/musicpro/player/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.musicpro.player.theme
+
+import androidx.compose.ui.graphics.Color
+
+object MusicProColors {
+    val Background = Color(0xFF0B0E14)
+    val Surface = Color(0xFF161922)
+    val SurfaceVariant = Color(0xFF1E2230)
+    val SurfaceVariantLight = Color(0xFF252A38)
+    val Primary = Color(0xFFE94560)
+    val onPrimary = Color(0xFFFFFFFF)
+    val PrimaryLight = Color(0xFFFF6B8A)
+    val PrimaryDark = Color(0xFFC73550)
+    val TextPrimary = Color(0xFFF0F0F5)
+    val TextSecondary = Color(0xFF888899)
+    val TextTertiary = Color(0xFF555566)
+    val Accent = Color(0xFF6366F1)
+    val Success = Color(0xFF22C55E)
+    val Warning = Color(0xFFF59E0B)
+    val Favorite = Color(0xFFE94560)
+
+    // Glassmorphism colors
+    val GlassSurface = Color(0x1AFFFFFF)
+    val GlassBorder = Color(0x20FFFFFF)
+    val GlassHighlight = Color(0x15FFFFFF)
+}
